fb5533ff427878f965bbee015f80bab000165c11
lofidewanto <lofidewanto@gmail.com>
2021-04-09 21:29:01 +0200

    Add Calculator microfrontend

---
+++ microfrontends-calculator/microfrontends-calculator-client/src/main/java/com/github/lofi/client/AppEntryPoint.java
@@ -0,0 +1,11 @@
+package com.github.lofi.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class AppEntryPoint implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// Do nothing, just to load the Java classes
+	}
+}
